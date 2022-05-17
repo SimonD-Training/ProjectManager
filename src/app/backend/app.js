fd6359fbf8041d9062ad09b55e6a23d751b3e0be
SimonDMaxwell <lolicongress@outlook.com>
2022-05-16 23:09:46 -0500

    Architecture Correction

--- src/app/backend/app.js
+++ src/app/backend/app.js
@@ -10,10 +10,10 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on port: ${port}..`));
 
 const api = require('./api/projectmanager.js');
-app.get("*.*", express.static("../../../dist/project-manager", { maxAge: "1y" })); //sets up the URIs for collecting the resources angular needs from the root of dist/projectmanager
+app.get("*.*", express.static("dist/project-manager", { maxAge: "1y" })); //sets up the URIs for collecting the resources angular needs from the root of dist/projectmanager
 app.post("*", api);
 app.put("*", api);
 app.delete("*", api);
 app.get("/*", (req, res) => {
-  res.status(200).sendFile(`/`, { root: "../../../dist/project-manager" });
+  res.status(200).sendFile(`/`, { root: "dist/project-manager" });
 });
